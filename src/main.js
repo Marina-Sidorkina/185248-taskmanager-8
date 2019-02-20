d0aa321aaceeac149d223bf97dcb2d46dee74b07
Marina Sidorkina <m.sidorkina55@gmail.com>
2019-02-20 21:46:24 +0300

    Add mistakes fixes

--- src/main.js
+++ src/main.js
@@ -1,56 +1,72 @@
 'use strict';
-const TASKS_BLOCK = document.querySelector(`.board__tasks`);
-const FILTERS_BLOCK = document.querySelector(`.main__filter`);
+
+const FITERS_NAMES = [`ALL`, `OVERDUE`, `TODAY`, `FAVORITES`, `Repeating`, `Tags`, `ARCHIVE`];
 const CARDS_NUMBER = 7;
+const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+const COLOR_BUTTONS_VARIANTS = [`black`, `yellow`, `blue`, `green`, `pink`];
+const CARD_STATUS_VALUES = [`edit`, `archive`, `favorites`];
 
-const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min) + min)
-;
+const CardParameter = {
+  CONTROLS: [`edit`, `archive`, `disabled`],
+  TEXT: `Here is a card with filled data`,
+  IMG_SRC: `img/sample-img.jpg`,
+  HASHTAGS: [`repeat`, `cinema`, `entertaiment`],
+  DEADLINE_TOGGLE_VALUE: `yes`,
+  DATE: `23 September`,
+  TIME: `11:15 PM`,
+  REPEAT_STATUS: `yes`
+};
 
-const renderControlButton = (status, value) =>
+const boardElement = document.querySelector(`.board__tasks`);
+const filterElement = document.querySelector(`.main__filter`);
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const renderControlButton = (status, value) => (
   `<button type="button" class="card__btn card__btn--${status}">
     ${value}
   </button>`
-;
+);
+
 const renderCardControlBlock = () => {
-  const STATUS_VARIANTS = [`edit`, `archive`, `disabled`];
-  const VALUES = [`edit`, `archive`, `favorites`];
   let controlButtonsBlock = ``;
-  for (let i = 0; i < VALUES.length; i++) {
-    controlButtonsBlock = controlButtonsBlock + renderControlButton(STATUS_VARIANTS[i], VALUES[i]);
-  }
-  return `<div class="card__control">
-    ${controlButtonsBlock}
-  </div>`;
+  CARD_STATUS_VALUES.forEach((value, index) => {
+    controlButtonsBlock += renderControlButton(CardParameter.CONTROLS[index], value);
+  });
+
+  return (
+    `<div class="card__control">
+      ${controlButtonsBlock}
+    </div>`
+  );
 };
 
-const renderColorBar = () =>
+const renderColorBar = () => (
   `<div class="card__color-bar">
     <svg class="card__color-bar-wave" width="100%" height="10">
       <use xlink:href="#wave"></use>
     </svg>
   </div>`
-;
+);
 
-const renderTextarea = () => {
-  const TEXT = `Here is a card with filled data`;
-  return `<div class="card__textarea-wrap">
+const renderTextarea = () => (
+  `<div class="card__textarea-wrap">
     <label>
       <textarea
         class="card__text"
         placeholder="Start typing your text here..."
-        name="text">${TEXT}</textarea>
+        name="text">${CardParameter.TEXT}</textarea>
     </label>
-  </div>`;
-};
+  </div>`
+);
 
-const renderDeadlineToggle = (setting) =>
+const renderDeadlineToggle = (setting) => (
   `<button class="card__date-deadline-toggle" type="button">
     date: <span class="card__date-status">${setting}</span>
   </button>`
-;
+);
 
-const renderDeadlineSetting = (parameter, setting) =>
+const renderDeadlineSetting = (parameter, setting) => (
   `<label class="card__input-deadline-wrap">
     <input
       class="card__${parameter}"
@@ -60,24 +76,27 @@ const renderDeadlineSetting = (parameter, setting) =>
       value="${setting}"
     />
   </label>`
-;
+);
 
 const renderDeadlineSettings = (dateSetting, timeSetting) => {
   const FIRST_PARAMETER = `date`;
   const SECOND_PARAMETER = `time`;
-  return `<fieldset class="card__date-deadline">
-  ${renderDeadlineSetting(FIRST_PARAMETER, dateSetting)}
-  ${renderDeadlineSetting(SECOND_PARAMETER, timeSetting)}
-  </fieldset>`;
+
+  return (
+    `<fieldset class="card__date-deadline">
+    ${renderDeadlineSetting(FIRST_PARAMETER, dateSetting)}
+    ${renderDeadlineSetting(SECOND_PARAMETER, timeSetting)}
+    </fieldset>`
+  );
 };
 
-const renderRepeatStatus = (status) =>
+const renderRepeatStatus = (status) => (
   `<button class="card__repeat-toggle" type="button">
     repeat:<span class="card__repeat-status">${status}</span>
   </button>`
-;
+);
 
-const renderRepeatDay = (day) =>
+const renderRepeatDay = (day) => (
   `<input
     class="visually-hidden card__repeat-day-input"
     type="checkbox"
@@ -88,35 +107,32 @@ const renderRepeatDay = (day) =>
   <label class="card__repeat-day" for="repeat-${day}-4"
     >${day}</label
   >`
-;
+);
 
 const renderRepeatDays = () => {
-  const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
-  let daysBlock = ``;
-  for (let i = 0; i < DAYS.length; i++) {
-    daysBlock = daysBlock + renderRepeatDay(DAYS[i]);
-  }
-  return `<fieldset class="card__repeat-days">
-    <div class="card__repeat-days-inner">
-      ${daysBlock}
-    </div>
-  </fieldset>`;
+  const block = DAYS
+    .map((day) => renderRepeatDay(day))
+    .join(``);
+
+  return (
+    `<fieldset class="card__repeat-days">
+      <div class="card__repeat-days-inner">
+        ${block}
+      </div>
+    </fieldset>`
+  );
 };
 
-const renderCardDatesBlock = () => {
-  const DEADLINE_TOGGLE_VALUE = `yes`;
-  const DATE_SETTING = `23 September`;
-  const TIME_SETTING = `11:15 PM`;
-  const REPEAT_STATUS = `yes`;
-  return `<div class="card__dates">
-  ${renderDeadlineToggle(DEADLINE_TOGGLE_VALUE)}
-  ${renderDeadlineSettings(DATE_SETTING, TIME_SETTING)}
-  ${renderRepeatStatus(REPEAT_STATUS)}
-  ${renderRepeatDays()}
-  </div>`;
-};
+const renderCardDatesBlock = () => (
+  `<div class="card__dates">
+    ${renderDeadlineToggle(CardParameter.DEADLINE_TOGGLE_VALUE)}
+    ${renderDeadlineSettings(CardParameter.DATE, CardParameter.TIME)}
+    ${renderRepeatStatus(CardParameter.REPEAT_STATUS)}
+    ${renderRepeatDays()}
+  </div>`
+);
 
-const renderColorButton = (color) =>
+const renderColorButton = (color) => (
   `<input
     type="radio"
     id="color-${color}-4"
@@ -129,40 +145,40 @@ const renderColorButton = (color) =>
     class="card__color card__color--${color}"
     >${color}</label
   >`
-;
+);
 
 const renderColorButtons = () => {
-  const COLOR_BUTTONS_VARIANTS = [`black`, `yellow`, `blue`, `green`, `pink`];
-  let buttonsBlock = ``;
-  for (let i = 0; i < COLOR_BUTTONS_VARIANTS.length; i++) {
-    buttonsBlock = buttonsBlock + renderColorButton(COLOR_BUTTONS_VARIANTS[i]);
-  }
-  return `<div class="card__colors-inner">
-    <h3 class="card__colors-title">Color</h3>
-      <div class="card__colors-wrap">
-        ${buttonsBlock}
+  const block = COLOR_BUTTONS_VARIANTS
+    .map((color) => renderColorButton(color))
+    .join(``);
+
+  return (
+    `<div class="card__colors-inner">
+      <h3 class="card__colors-title">Color</h3>
+        <div class="card__colors-wrap">
+          ${block}
+        </div>
       </div>
-    </div>
-  </div>`;
+    </div>`
+  );
 };
 
-const renderImage = () => {
-  const SRC = `img/sample-img.jpg`;
-  return `<label class="card__img-wrap">
+const renderImage = () => (
+  `<label class="card__img-wrap">
     <input
       type="file"
       class="card__img-input visually-hidden"
       name="img"
     />
     <img
-      src="${SRC}"
+      src="${CardParameter.IMG_SRC}"
       alt="task picture"
       class="card__img"
     />
-  </label>`;
-};
+  </label>`
+);
 
-const renderHashtag = (value) =>
+const renderHashtag = (value) => (
   `<span class="card__hashtag-inner">
     <input
       type="hidden"
@@ -177,9 +193,9 @@ const renderHashtag = (value) =>
       delete
     </button>
   </span>`
-;
+);
 
-const renderHashtagInput = () =>
+const renderHashtagInput = () => (
   `<label>
     <input
       type="text"
@@ -188,30 +204,31 @@ const renderHashtagInput = () =>
       placeholder="Type new hashtag here"
     />
   </label>`
-;
+);
 
 const renderHashtagsBlock = () => {
-  const HASHTAGS = [`repeat`, `cinema`, `entertaiment`];
-  let hashtagsBlock = ``;
-  for (let i = 0; i < HASHTAGS.length; i++) {
-    hashtagsBlock = hashtagsBlock + renderHashtag(HASHTAGS[i]);
-  }
-  return `<div class="card__hashtag">
-    <div class="card__hashtag-list">
-      ${hashtagsBlock}
-    </div>
-    ${renderHashtagInput()}
-  </div>`;
+  const block = CardParameter.HASHTAGS
+    .map((hashtag) => renderHashtag(hashtag))
+    .join(``);
+
+  return (
+    `<div class="card__hashtag">
+      <div class="card__hashtag-list">
+        ${block}
+      </div>
+      ${renderHashtagInput()}
+    </div>`
+  );
 };
 
-const renderStatusBtns = () =>
+const renderStatusBtns = () => (
   `<div class="card__status-btns">
     <button class="card__save" type="submit">save</button>
     <button class="card__delete" type="button">delete</button>
   </div>`
-;
+);
 
-const renderCard = () =>
+const renderCard = () => (
   `<article class="card card--edit card--yellow card--repeat">
     <form class="card__form" method="get">
       <div class="card__inner">
@@ -229,49 +246,55 @@ const renderCard = () =>
       </div>
     </form>
   </article>`
-;
+);
 
 const renderCardsList = (number) => {
   let cardsBlock = ``;
   for (let i = 0; i < number; i++) {
-    cardsBlock = cardsBlock + renderCard();
+    cardsBlock += renderCard();
   }
+
   return cardsBlock;
 };
 
 const renderFilter = (name, status) => {
-  const NUMBER = getRandomNumber(1, 15);
-  return `<input
-    type="radio"
-    id="filter__${name.toLowerCase()}"
-    class="filter__input visually-hidden"
-    name="filter"
-    value="${NUMBER}"
-    ${status}
-  />
-  <label for="filter__${name.toLowerCase()}" class="filter__label">
-    ${name} <span class="filter__all-count">${NUMBER}</span></label
-  >`;
+  const number = getRandomNumber(1, 15);
+
+  return (
+    `<input
+      type="radio"
+      id="filter__${name.toLowerCase()}"
+      class="filter__input visually-hidden"
+      name="filter"
+      value="${number}"
+      ${status}
+    />
+    <label for="filter__${name.toLowerCase()}" class="filter__label">
+      ${name} <span class="filter__all-count">${number}</span></label
+    >`
+  );
 };
+
 const renderFiltersBlock = () => {
-  const NAMES = [`ALL`, `OVERDUE`, `TODAY`, `FAVORITES`, `Repeating`, `Tags`, `ARCHIVE`];
-  let filtersBlock = ``;
-  for (let i = 0; i < NAMES.length; i++) {
-    let status = NAMES[i] === `TODAY` ? `checked` : ``;
-    filtersBlock = filtersBlock + renderFilter(NAMES[i], status);
-  }
-  return filtersBlock;
+  let block = ``;
+  FITERS_NAMES.forEach((name) => {
+    let status = name === `TODAY` ? `checked` : ``;
+    block += renderFilter(name, status);
+  });
+
+  return block;
 };
 
 const addFiltersEventListener = () => {
-  const FILTERS = document.querySelectorAll(`.filter__input`);
-  FILTERS.forEach((item) => {
+  const filterInputElements = document.querySelectorAll(`.filter__input`);
+  filterInputElements.forEach((item) => {
     item.addEventListener(`click`, () => {
-      TASKS_BLOCK.innerHTML = renderCardsList(item.value);
+      boardElement.innerHTML = renderCardsList(item.value);
     });
   });
 };
 
-FILTERS_BLOCK.innerHTML = renderFiltersBlock();
-TASKS_BLOCK.innerHTML = renderCardsList(CARDS_NUMBER);
+filterElement.innerHTML = renderFiltersBlock();
+boardElement.innerHTML = renderCardsList(CARDS_NUMBER);
+
 addFiltersEventListener();
